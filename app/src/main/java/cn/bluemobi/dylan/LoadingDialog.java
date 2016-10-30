1114613237dc6362e9f8e37cd957568f4fd66144
linglongxin24 <linglongxin24@163.com>
2016-10-30 11:20:05 +0800

    精简并优化代码

--- app/src/main/java/cn/bluemobi/dylan/LoadingDialog.java
+++ app/src/main/java/cn/bluemobi/dylan/LoadingDialog.java
@@ -2,9 +2,12 @@ package cn.bluemobi.dylan;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.widget.TextView;
 
 /**
+ * 自定义加载进度对话框
  * Created by Administrator on 2016-10-28.
  */
 
@@ -12,12 +15,20 @@ public class LoadingDialog extends Dialog {
     private TextView tv_text;
 
     public LoadingDialog(Context context) {
-        super(context, R.style.transparentFrameWindowStyle);
+        super(context);
+        /**设置对话框背景透明*/
+        getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         setContentView(R.layout.loading);
         tv_text = (TextView) findViewById(R.id.tv_text);
         setCanceledOnTouchOutside(false);
     }
 
+    /**
+     * 为加载进度个对话框设置不同的提示消息
+     *
+     * @param message 给用户展示的提示信息
+     * @return build模式设计，可以链式调用
+     */
     public LoadingDialog setMessage(String message) {
         tv_text.setText(message);
         return this;
